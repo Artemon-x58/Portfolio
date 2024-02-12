ca5daf07b824d2947204f524151eaaf1950e1fae
Artemon-x58 <artem1145819@gmail.com>
2024-02-12 20:48:32 +0100

    update menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { MenuBtn, MenuBurger, MenuContainer, MenuExit } from "./Header.styled";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <MenuContainer>
-        <MenuBtn>
-          <MenuBurger />
-          <MenuExit />
+        <MenuBtn onClick={handleIsOpen}>
+          <MenuBurger isOpen={isOpen} />
+          <MenuExit isOpen={isOpen} />
         </MenuBtn>
       </MenuContainer>
     </>
